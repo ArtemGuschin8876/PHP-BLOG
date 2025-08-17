110b050ec4df410e3f6ddf994d0dc26689125b9e
ArtemGuschin8876 <newmsiartem@gmail.com>
2025-08-17 19:22:07 +0400

    fix phpstan, fixer, rector

--- migrations/Version20241115175716.php
+++ migrations/Version20241115175716.php
@@ -20,6 +20,7 @@ final class Version20241115175716 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('ALTER TABLE post RENAME TO posts;');
+        //dasd
     }
 
     public function down(Schema $schema): void
